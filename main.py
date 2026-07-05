0ad2bfb534598b97947c038fe3105070bcc75f04
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-07-06 02:15:38 +0530

    Add `about` endpoint to provide project details in the Student Management API

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ def home():
         "message": "Welcome to Student Management API, build by Sharma Enterprises."
     }
 
+@app.get("/about")
+def about():
+    return {
+        "project-name": "Student Management System",
+        "created-by": "Sharma Enterprises",
+        "developer": "Ganesh Sharma",
+        "current-version": "v1.0"
+    }
+
