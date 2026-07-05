5b73dd02216f8e51d5012c04c2e41caa9a8f373e
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-07-06 02:21:10 +0530

    Update API endpoints to include versioning and improve structure

--- main.py
+++ main.py
@@ -3,18 +3,26 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get(path = "/")
+@app.get(path = "/api/")
 def home():
     return {
         "message": "Welcome to Student Management API, build by Sharma Enterprises."
     }
 
-@app.get("/about")
+@app.get("/api/about")
 def about():
     return {
         "project-name": "Student Management System",
         "created-by": "Sharma Enterprises",
         "developer": "Ganesh Sharma",
         "current-version": "v1.0"
+    }
+
+@app.get("/api/contact")
+def contact():
+    return {
+        "email": "ganesh27sharma09@gmail.com",
+        "mobile": "90xxx-99xxx",
+        "website": "https://ganeshsharma.tech"
     }
 
