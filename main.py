048e6a949dfcd97f28fcb77295d793a306509e91
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-07-06 02:46:17 +0530

    Add STUDENT_DATA and update student_info endpoint to return student details

--- main.py
+++ main.py
@@ -3,11 +3,46 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+STUDENT_DATA = [
+    {
+        "id": 101,
+        "name": "Ganesh Sharma",
+        "age": 22,
+        "sex": "Male",
+        "latest_qualification": "Undergraduate"
+    },
+    {
+        "id": 102,
+        "name": "Preeti Sharma",
+        "age": 21,
+        "sex": "Female",
+        "latest_qualification": "Undergraduate"
+    },
+    {
+        "id": 103,
+        "name": "Rekha Jain",
+        "age": 26,
+        "sex": "Female",
+        "latest_qualification": "Postgraduate"
+    }
+]
+
 @app.get(path = "/")
 def home():
     return {
         "message": "Welcome to Student Management API, build by Sharma Enterprises."
     }
+
+@app.get("/student/{student_id}")
+def student_info(student_id: int):
+    if student_id == 101:
+        return {
+            "id": 101,
+            "name": "Ganesh Sharma",
+            "age": 22,
+            "sex": "Male",
+            "latest_qualification": "Undergraduate"
+        }
 
 @app.get("/about")
 def about():
