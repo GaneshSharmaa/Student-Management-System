3832a15d25311b57e50adead9e4e310174e096fc
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-07-06 02:26:04 +0530

    Refactor API endpoints to remove versioning and standardize response keys

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get(path = "/api/")
+@app.get(path = "/")
 def home():
     return {
         "message": "Welcome to Student Management API, build by Sharma Enterprises."
     }
 
-@app.get("/api/about")
+@app.get("/about")
 def about():
     return {
-        "project-name": "Student Management System",
-        "created-by": "Sharma Enterprises",
+        "project_name": "Student Management System",
+        "created_by": "Sharma Enterprises",
         "developer": "Ganesh Sharma",
-        "current-version": "v1.0"
+        "current_version": "v1.0"
     }
 
-@app.get("/api/contact")
+@app.get("/contact")
 def contact():
     return {
         "email": "ganesh27sharma09@gmail.com",
