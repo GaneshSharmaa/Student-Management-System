353735c4d4970f8542a7b3eb15558e4887ac0d6f
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-07-07 01:23:40 +0530

    Add student and semester data, and update student info endpoint for improved data retrieval

--- main.py
+++ main.py
@@ -1,49 +1,40 @@
-from fastapi import FastAPI
+# importing the modules
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 
+# importing the data from the file
+from student_information.student_data import STUDENT_DATA
+from student_information.semester_data import SEMESTER_1
+
+# initializing the application
 app = FastAPI()
 
-STUDENT_DATA = [
-    {
-        "id": 101,
-        "name": "Ganesh Sharma",
-        "age": 22,
-        "sex": "Male",
-        "latest_qualification": "Undergraduate"
-    },
-    {
-        "id": 102,
-        "name": "Preeti Sharma",
-        "age": 21,
-        "sex": "Female",
-        "latest_qualification": "Undergraduate"
-    },
-    {
-        "id": 103,
-        "name": "Rekha Jain",
-        "age": 26,
-        "sex": "Female",
-        "latest_qualification": "Postgraduate"
-    }
-]
-
+# HOME PAGE ROUTE
 @app.get(path = "/")
 def home():
     return {
         "message": "Welcome to Student Management API, build by Sharma Enterprises."
     }
 
+# EACH STUDENT INFO PAGE ROUTE
 @app.get("/student/{student_id}")
 def student_info(student_id: int):
-    if student_id == 101:
-        return {
-            "id": 101,
-            "name": "Ganesh Sharma",
-            "age": 22,
-            "sex": "Male",
-            "latest_qualification": "Undergraduate"
-        }
+    for each_student in STUDENT_DATA:
+        if student_id == each_student["id"]: 
+            return each_student
 
+    raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Student with ID {student_id} not found!")
+
+# EACH SEMESTER DETAILS PAGE ROUTE
+@app.get("/student/{student_id}/semester/{semester}")
+def student_semester_detail(student_id: int, semester: int):
+    for each_student_marks in SEMESTER_1:
+        if student_id == each_student_marks["id"]:
+            return student_info(student_id) | each_student_marks
+    
+    raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Student with ID {student_id} not found!")
+
+# ABOUT PAGE ROUTE
 @app.get("/about")
 def about():
     return {
@@ -53,6 +44,7 @@ def about():
         "current_version": "v1.0"
     }
 
+# CONTACT PAGE ROUTE
 @app.get("/contact")
 def contact():
     return {
