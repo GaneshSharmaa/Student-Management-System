77b5ac22ef8274c326e2d120a2d740bad644ee10
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-07-11 01:03:29 +0530

    Add `StudentPartialUpdate` schema for partial updates to student information

--- schemas/student.py
+++ schemas/student.py
@@ -7,3 +7,9 @@ class Student(BaseModel):
     sex: str
     latest_qualification: str
 
+class StudentPartialUpdate(BaseModel):
+    name: str | None = None     # this None = None, means it has a default value None
+    age: int | None = None
+    sex: str | None = None
+    latest_qualification: str | None = None
+
