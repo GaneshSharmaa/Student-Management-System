30c2b85dc2ac664239bbb9650cc8730f57aea0b5
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-07-13 19:00:12 +0530

    Add session maker to database setup for request handling

--- database/database.py
+++ database/database.py
@@ -1,5 +1,6 @@
 # importing the modules
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
@@ -11,4 +12,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 # creating an engine
 engine = create_engine(DATABASE_URL)
+
+# creating a session maker — this would create sessions on every request
+SessionLocal = sessionmaker(bind = engine)
 
