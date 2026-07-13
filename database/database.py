256b7eeb0620bd0e56d9ba88b47acef62db24af6
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-07-13 19:03:24 +0530

    Add DeclarativeBase for ORM infrastructure and create student model file

--- database/database.py
+++ database/database.py
@@ -1,6 +1,6 @@
 # importing the modules
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from dotenv import load_dotenv
 import os
 
@@ -15,4 +15,9 @@ engine = create_engine(DATABASE_URL)
 
 # creating a session maker — this would create sessions on every request
 SessionLocal = sessionmaker(bind = engine)
+
+# `DeclarativeBase` provides the ORM infrastructure such as metadata, registry
+# if we used DeclarativeBase everytime then SQLAlchemy will treat all those models as different registries
+class Base(DeclarativeBase):
+    pass
 
