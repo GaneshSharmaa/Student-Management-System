b9547a461e8210dfe08608c3b556f19250aae617
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-07-15 13:14:34 +0530

    Add `Semester` model to define semester-related data structure

--- models/student.py
+++ models/student.py
@@ -17,3 +17,11 @@ class Student(Base):
     sex: Mapped[str] = mapped_column(String(8), nullable = False)
     latest_qualification: Mapped[str] = mapped_column(String(20), nullable = False)
 
+class Semester(Base):
+    __tablename__ = "semester"
+
+    semester_id: Mapped[int] = mapped_column(primary_key = True)
+    student_id: Mapped[int] = mapped_column(nullable = False)
+    subject: Mapped[str] = mapped_column(String(25), nullable = False)
+    marks: Mapped[int] = mapped_column(Integer(min = 0, max = 100), nullable = False)
+
