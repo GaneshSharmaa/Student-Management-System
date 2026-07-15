16c130c5d16d9464ede290cb6c797579459309c3
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-07-15 13:30:19 +0530

    Refactor main application routes to enhance structure and add about/contact endpoints with project details

--- main.py
+++ main.py
@@ -23,23 +23,45 @@ Base.metadata.create_all(bind = engine)
 # initializing the application
 app = FastAPI()
 
-# HOME PAGE ROUTE
+# ------------ HOME PAGE ROUTE ------------
 @app.get(path = "/")
 def home():
     return {
         "message": "Welcome to Student Management API, build by Sharma Enterprises."
     }
 
-# EACH STUDENT INFO PAGE ROUTE
+# ------------ ABOUT PAGE ROUTE ------------
+@app.get("/about")
+def about():
+    return {
+        "project_name": "Student Management System",
+        "created_by": "Sharma Enterprises",
+        "developer": "Ganesh Sharma",
+        "current_version": "v1.0"
+    }
+
+# ----------- CONTACT PAGE ROUTE -----------
+@app.get("/contact")
+def contact():
+    return {
+        "email": "ganesh27sharma09@gmail.com",
+        "mobile": "90xxx-99xxx",
+        "website": "https://ganeshsharma.tech"
+    }
+
+# ---------- EACH STUDENT INFO PAGE ROUTE ----------
 @app.get("/student/{student_id}")
-def student_info(student_id: int):
-    for each_student in STUDENT_DATA:
-        if student_id == each_student["id"]: 
-            return each_student
+def student_info(student_id: int, db: Session = Depends(get_db)):
+    statement = select(StudentModel).where(StudentModel.id == student_id)
+    result = db.execute(statement)
+    student = result.scalars().first()
 
-    raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Student with ID {student_id} not found!")
+    if student is None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Student with ID {student_id} not found!")
+    
+    return student
 
-# QUERY PARAMETER FOR FILTERING BASED ON SEX
+# --------- QUERY PARAMETER FOR FILTERING BASED ON SEX ---------
 @app.get("/students")
 def get_students(sex: str | None = None, latest_qualification: str | None = None, db: Session = Depends(get_db)):
     statement = select(StudentModel)   # the database statement
@@ -63,25 +85,6 @@ def student_semester_detail(student_id: int, semester: int):
             return student_info(student_id) | each_student_marks
 
     raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Marks of Semester {semester} ID {student_id} not found!")
-
-# ABOUT PAGE ROUTE
-@app.get("/about")
-def about():
-    return {
-        "project_name": "Student Management System",
-        "created_by": "Sharma Enterprises",
-        "developer": "Ganesh Sharma",
-        "current_version": "v1.0"
-    }
-
-# CONTACT PAGE ROUTE
-@app.get("/contact")
-def contact():
-    return {
-        "email": "ganesh27sharma09@gmail.com",
-        "mobile": "90xxx-99xxx",
-        "website": "https://ganeshsharma.tech"
-    }
 
 # HELPER FUNCTION FOR FINDING OUT HIGHEST ID AND THEN INCREMENTING IT BY 1
 def new_id(DATA):
