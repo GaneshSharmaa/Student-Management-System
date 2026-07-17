051f6f7f89067bd8f932b41748796b036e13ac8e
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-07-17 21:48:39 +0530

    Enhance relationships in `Student` and `SemesterMarks` models for improved ORM mapping

--- models/student.py
+++ models/student.py
@@ -3,28 +3,38 @@ from database.database import Base
 
 # importing necessary modules
 from sqlalchemy import Integer, String, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 # `mapped_column` represents database column
 # `Mapped` is used for ORM mapping
 
 # database model for student
 class Student(Base):
+    # table name
     __tablename__ = "students"
 
+    # columns
     id: Mapped[int] = mapped_column(primary_key = True)
     name: Mapped[str] = mapped_column(String(50), nullable = False)
     age: Mapped[int] = mapped_column(nullable = False)
     sex: Mapped[str] = mapped_column(String(8), nullable = False)
     latest_qualification: Mapped[str] = mapped_column(String(20), nullable = False)
 
+    # relationship
+    semester_marks = relationship("SemesterMarks")
+
 # database model for semester data
 class SemesterMarks(Base):
+    # table name
     __tablename__ = "semester"
 
+    # columns
     semester_id: Mapped[int] = mapped_column(primary_key = True)
     student_id: Mapped[int] = mapped_column(ForeignKey("students.id"), nullable = False)
     subject: Mapped[str] = mapped_column(String(25), nullable = False)
     marks: Mapped[int] = mapped_column(Integer, nullable = False)
     semester: Mapped[int] = mapped_column(Integer, nullable = False)
+
+    # relationship
+    student = relationship("Student")
 
