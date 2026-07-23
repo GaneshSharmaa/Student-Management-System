c6e0fad149c29df90b3306fae947b3a2bb74d1d7
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-07-24 03:30:13 +0530

    Add `UserRegister` and `UserResponse` models for user registration and response handling

---
+++ schemas/user.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, ConfigDict
+
+class UserRegister(BaseModel):
+    username: str
+    email: str
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: str
+
+    model_config = ConfigDict(from_attributes = True)
+
