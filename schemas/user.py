98a1bfcb6c1746dfc8fe5ab24c109afd44badf35
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-07-24 15:07:05 +0530

    Add `UserLogin` schema for user login validation and comments for previously existing schemas

--- schemas/user.py
+++ schemas/user.py
@@ -1,14 +1,22 @@
+# importing the modules
 from pydantic import BaseModel, ConfigDict
 
+# schema for user registeration validation
 class UserRegister(BaseModel):
     username: str
     email: str
     password: str
 
+# schema for user response validation
 class UserResponse(BaseModel):
     id: int
     username: str
     email: str
 
     model_config = ConfigDict(from_attributes = True)
+
+# schema for user login validation
+class UserLogin(BaseModel):
+    email: str
+    password: str
 
