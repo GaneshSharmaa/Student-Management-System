cb9e4109168f138d10e16fb47c8b3446f7f6bf28
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-07-24 20:56:12 +0530

    Add `verify_access_token` function for JWT token validation

--- auth/jwt.py
+++ auth/jwt.py
@@ -1,5 +1,7 @@
 # importing necessary modules
 import jwt
+from jwt import InvalidTokenError
+from fastapi import HTTPException, status
 from dotenv import load_dotenv
 import os
 from datetime import datetime, timedelta, timezone
@@ -16,6 +18,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 if SECRET_KEY is None:
     raise ValueError("SECRET_KEY environment variable not set.")
 
+# function to generate signed access token
 def create_access_token(data: dict) -> str:
     # making a copy of the data dictionary
     to_encode = data.copy()
@@ -30,4 +33,28 @@ def create_access_token(data: dict) -> str:
     jwt_string = jwt.encode(payload = to_encode, key = SECRET_KEY, algorithm = ALGORITHM)
 
     return jwt_string
+
+# function to decode the signed token and extract useful information
+def verify_access_token(token: str) -> str:
+    try:
+        # decoding the token to get payload
+        payload = jwt.decode(jwt = token, key = SECRET_KEY, algorithms = [ALGORITHM])
+
+        # extracting the `sub` claim from the payload
+        sub = payload.get("sub")
+
+        # check if claim is not there, raise exception
+        if sub is None:
+            raise HTTPException(
+                status_code = status.HTTP_401_UNAUTHORIZED,
+                detail = "Could not validate credentials"
+            )
+
+        return payload
+
+    except InvalidTokenError:
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = "Could not validate credentials"
+        )
 
